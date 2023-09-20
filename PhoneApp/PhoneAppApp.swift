91393706011cd5f29f2b503def086f7279bc2c49
Hilal <hilalparlakci>
2023-09-20 17:19:24 +0300

    Initial Commit

---
+++ PhoneApp/PhoneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneAppApp.swift
+//  PhoneApp
+//
+//  Created by Hilal on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhoneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
